4d9e46277f87cf24e0e155692d02bff4414e01bb
papinovosel <mateo.novosel2@gmail.com>
2024-11-05 22:56:10 +0100

    Separated Reactions from PostsController

---
+++ Api/Controllers/ReactionsController.cs
@@ -0,0 +1,53 @@
+﻿using Core.Data.Dto.PostDto;
+using Core.Enums;
+using Core.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/reactions/")]
+    public class ReactionsController : ControllerBase
+    {
+        private readonly IPostReactionsService _postReactionsService;
+
+        public ReactionsController(IPostReactionsService postReactionsService)
+        {
+            _postReactionsService = postReactionsService;
+        }
+
+
+
+        // POST: /api/reactions/posts/add/31faddd4-c910-45c2-a68b-bf67b5abaa77/
+
+        [HttpPost("posts/add/{postId}")]
+        public async Task<IActionResult> AddPostReaction(Guid postId, ReactionType reaction)
+        {
+            await _postReactionsService.AddPostReactionAsync(postId, reaction);
+
+            return Ok();
+        }
+
+        // PUT: /api/reactions/posts/update/31faddd4-c910-45c2-a68b-bf67b5abaa77/
+
+        [HttpPut("posts/update/{postId}")]
+        public async Task<IActionResult> UpdatePostReaction(Guid postId)
+        {
+            await _postReactionsService.UpdatePostReaction(postId);
+
+            return Ok();
+        }
+
+        // DELETE /api/reactions/posts/remove/31faddd4-c910-45c2-a68b-bf67b5abaa77/
+
+        [HttpDelete("posts/delete/{postId}")]
+        public async Task<IActionResult> DeletePostReaction(Guid postId)
+        {
+            await _postReactionsService.DeletePostReaction(postId);
+
+            return Ok();
+        }
+    }
+}
--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8d71cc80daf9b5ee6ccca6b5ba62abd2bc6520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f951e61f3028217c539ae37b163c6c9b25d022")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
