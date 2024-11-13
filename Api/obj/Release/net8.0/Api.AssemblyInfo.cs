30747cea5f2f5df4a14962e53fc7599e2013d247
papinovosel <mateo.novosel2@gmail.com>
2024-11-13 01:14:38 +0100

    random

--- Api/obj/Release/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Release/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace2707e3743b25b73837cdb4429a36c7a37420e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28565577db631ca744c0d6cb68520b84cbcd2c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f0bf5019fef177e7423aeb528c63bd1224beb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d66df16f07c227ba5568b19159fc7cb7ed5798c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
