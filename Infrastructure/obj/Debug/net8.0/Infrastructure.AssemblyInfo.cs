2d66df16f07c227ba5568b19159fc7cb7ed5798c
papinovosel <mateo.novosel2@gmail.com>
2024-11-13 01:09:35 +0100

    Remove PublishProfiles

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace2707e3743b25b73837cdb4429a36c7a37420e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f0bf5019fef177e7423aeb528c63bd1224beb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
