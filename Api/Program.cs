c5fce19114c07a33b30563832c5220df8c249078
papinovosel <mateo.novosel2@gmail.com>
2025-01-23 23:55:25 +0100

    New .gitignore

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,9 +9,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHsts();
 app.UseHttpsRedirection();
